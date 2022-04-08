d786529789f6a4e8ecec8ecc9337f5c854dda43c
=Eric Niemeyer <=niemeyer.eric@gmail.com>
2022-04-07 21:41:34 -0400

    init

---
+++ Program.cs
@@ -0,0 +1,12 @@
+﻿using System;
+
+namespace assignment_wizard_ninja_samurai
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            Console.WriteLine("Hello World!");
+        }
+    }
+}
---
+++ Wizard.cs
@@ -0,0 +1,15 @@
+using System;
+
+namespace assignment_wizard_ninja_samurai
+{
+
+  class Wizard : Human
+  {
+    public Wizard(string n) : base(n, ){
+      Intelligence = 25;
+      health = 50;
+    }
+
+  }
+
+}
